aca73bfce04c16c08c8da58369f9419e27b2a546
gongyantong <chronus1125@163.com>
2022-10-04 16:03:13 +0800

    Lab1 commit

---
+++ src/main/java/practice/lab1/Hello.java
@@ -0,0 +1,13 @@
+package practice.lab1;
+
+/**.
+ * Hello world
+ *
+ * @author gongyantong
+ * @since 2022-10-04
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
